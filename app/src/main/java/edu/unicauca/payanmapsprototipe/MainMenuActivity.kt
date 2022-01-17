7b6d7b6c904097c55d2ebaab24e5c53eba035649
Ark99 <32331814+Arcaurus99@users.noreply.github.com>
2022-01-17 17:15:45 -0500

    SplassCreen - Ready: No allowed
    Icons - Setted:
    All icons are created.
    
    Layouts:
    Images do not displayed
    OnClick needed to change in layouts

--- app/src/main/java/edu/unicauca/payanmapsprototipe/MainMenuActivity.kt
+++ app/src/main/java/edu/unicauca/payanmapsprototipe/MainMenuActivity.kt
@@ -5,7 +5,17 @@ import android.os.Bundle
 
 class MainMenuActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //setTheme(R.style.SplashTheme)
+
+        var count = 0
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        fun onTap(viewt Viwe) {
+
+        }
+
     }
 }
