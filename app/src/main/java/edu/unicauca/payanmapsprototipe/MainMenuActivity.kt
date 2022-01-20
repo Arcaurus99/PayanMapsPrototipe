55d0a5082c2d10244ffc2d1e23635b76ce578c65
Ark99 <32331814+Arcaurus99@users.noreply.github.com>
2022-01-20 17:14:36 -0500

    - Plugin added - Importing items from layouts ready. - Function to change layout in process - Function to prove the buttons added

--- app/src/main/java/edu/unicauca/payanmapsprototipe/MainMenuActivity.kt
+++ app/src/main/java/edu/unicauca/payanmapsprototipe/MainMenuActivity.kt
@@ -2,20 +2,23 @@ package edu.unicauca.payanmapsprototipe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main_menu.*
 
 class MainMenuActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
 
         //setTheme(R.style.SplashTheme)
 
-        var count = 0
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
 
-        fun onTap(viewt Viwe) {
-
+        btnTourism.setOnClickListener {
+            Toast.makeText(this, "You clicked Tourism", Toast.LENGTH_SHORT).show()
         }
+
+
 
     }
 }
