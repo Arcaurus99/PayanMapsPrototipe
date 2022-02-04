0dec8fd5bde82fa0b563bbc5b987f09a317c2f51
andrescalderon90 <96318972+andrescalderon90@users.noreply.github.com>
2022-02-04 15:40:07 -0500

    events
    creacion de pantalla events

---
+++ app/src/main/java/edu/unicauca/payanmapsprototipe/EventsActivity.java
@@ -0,0 +1,14 @@
+package edu.unicauca.payanmapsprototipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events);
+    }
+}
